590273a06270844e9389f384fef38d6b96d3f2fd
zijun_he <only_slient.com>
2021-12-24 17:52:03 +0800

    feat: nginx,minio

--- server/docker-compose/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cw=`pwd`
-
-export GITLAB_HOME=$cw/gitlab
-export GITLAB_RUNNER_HOME=$cw/gitlab-runner
-
-docker-compose up -d
---
+++ server/docker-compose/scp.sh
@@ -0,0 +1,2 @@
+
+scp -r ./* root@81.68.217.99:/opt/infra
